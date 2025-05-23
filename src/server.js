f48fa41b5bcc9144e29c26d0d3363edac701ef99
vijit-vishnoi <vishnoivijit@gamil.com>
2025-05-23 15:51:13 +0530

    Did some changes in server.js

--- src/server.js
+++ src/server.js
@@ -10,4 +10,7 @@ const schoolRoutes = require('./routes/schoolRoutes');
 app.use('/api/schools', schoolRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
