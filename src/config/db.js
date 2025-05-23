06a8a21696838409c206f7f4eb8e38e269cdc4fb
vijit-vishnoi <vishnoivijit@gamil.com>
2025-05-23 13:26:21 +0530

    Created .env file

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'vijit',
-  database: 'school_db',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
 });
