5ca247d3b8adc61e84b72d2dd6f5e1b38a04e904
vijit-vishnoi <vishnoivijit@gamil.com>
2025-05-23 16:44:00 +0530

    Added port in .env

--- src/config/db.js
+++ src/config/db.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
