46c45ac666ec055783ed435c46429038b7801b00
vijit-vishnoi <vishnoivijit@gamil.com>
2025-05-23 14:11:02 +0530

    Did some changes for hosting the project

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
   waitForConnections: true,
   connectionLimit: 10,
 });
